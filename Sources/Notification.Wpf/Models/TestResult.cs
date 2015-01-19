183eaa919f2dfbf5e7efd45b752bc496e4559d08
Peijen Lin <peijen.lin@live.com>
2015-01-19 00:10:35 -0800

    missing file ignored by .gitignore

---
+++ Sources/Notification.Wpf/Models/TestResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Wpf.Models
+{
+    public class TestResult
+    {
+        public string Method { get; set; }
+        public int ObjectCount { get; set; }
+        public int IterationCount { get; set; }
+        public long ConstructionMilliseconds { get; set; }
+        public long ExecutionMilliseconds { get; set; }
+        public long TotalMilliseconds { get { return ConstructionMilliseconds + ExecutionMilliseconds; } }
+    }
+}
